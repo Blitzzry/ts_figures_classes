8ef963a662b62bf6020fa9de51953842b3b74bc0
Juan Camilo García Uribe <juancamilogarciauribe@MacBook-Pro-von-Juan.local>
2026-05-05 23:23:52 -0500

    ahora sii

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -4,53 +4,82 @@ export interface Figure {
 }
 
 export class Triangle implements Figure {
-  constructor(public color: string, public a: number, public b: number, public c: number) {
-    let sumArray: number[] = [this.a, this.b, this.c]
+  constructor(
+    public color: string,
+    public x: number,
+    public y: number,
+    public c: number,
+  ) {
+    const sumArray: number[] = [this.x, this.y, this.c];
+
     if (sumArray.sort((a, b) => b - a)[0] >= sumArray[1] + sumArray[2]) {
-      throw new Error('Invalid triangle: sum of any two sides must be greater than the third side');
+      throw new Error(
+        'The longest side of a triangle cannot be greater than or equal to the sum of the other two sides',
+      );
     }
-    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
-    throw new Error('Side length must be greater than 0');
+
+    if (this.x <= 0 || this.y <= 0 || this.c <= 0) {
+      throw new Error('Side length must be greater than 0');
+    }
   }
-  }
-  shape = "triangle";
+
+  shape = 'triangle';
+
   getInfo(): string {
     return `A ${this.color} ${this.shape} - ${this.getArea()}`;
   }
+
   getArea(): number {
-    
-    const p = (this.a + this.b + this.c) / 2;
-    return Math.floor(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)) * 100) / 100 ;
+    const p = (this.x + this.y + this.c) / 2;
+
+    return (
+      Math.floor(
+        Math.sqrt(p * (p - this.x) * (p - this.y) * (p - this.c)) * 100,
+      ) / 100
+    );
   }
 }
 
 export class Circle implements Figure {
-  constructor(public color: string, public radius: number) {
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
     if (this.radius <= 0) {
       throw new Error('Radius must be greater than 0');
     }
   }
-  shape = "circle";
+
+  shape = 'circle';
+
   getInfo(): string {
     return `A ${this.color} ${this.shape} - ${this.getArea()}`;
   }
+
   getArea(): number {
-    return Math.floor((Math.PI * Math.pow(this.radius, 2)) * 100) / 100 ;
+    return Math.floor(Math.PI * Math.pow(this.radius, 2) * 100) / 100;
   }
 }
 
 export class Rectangle implements Figure {
-  constructor(public color: string, public width: number, public height: number) {
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
     if (this.width <= 0 || this.height <= 0) {
       throw new Error('Width and height must be greater than 0');
     }
   }
-  shape = "rectangle";
+
+  shape = 'rectangle';
+
   getInfo(): string {
     return `A ${this.color} ${this.shape} - ${this.getArea()}`;
   }
+
   getArea(): number {
-    return Math.floor((this.width * this.height) * 100) / 100 ;
+    return Math.floor(this.width * this.height * 100) / 100;
   }
 }
 
